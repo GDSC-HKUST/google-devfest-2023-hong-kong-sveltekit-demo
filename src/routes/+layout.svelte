42028186ed2c487eeaeb2c3b3338c4b4ecf5bff9
ColaIan <colaian.yt@gmail.com>
2023-11-18 10:26:17 +0800

    Add basic styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,8 @@
-<p>Demo Comment App</p>
-<slot />
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto p-4 space-y-4">
+	<p>Demo Comment App</p>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,24 @@
 
 {#if $page.data.session?.user}
 	Signed in as {$page.data.session.user.name} ({$page.data.session.user.email})
-	<button on:click={() => signOut()}>Sign Out</button>
+	<button on:click={() => signOut()} class="py-1.5 px-2.5 rounded-md">Sign Out</button>
 {:else}
-	<button on:click={() => signIn('google')}>Sign In</button>
+	<button on:click={() => signIn('google')} class="border py-1.5 px-2.5 rounded-md">Sign In</button>
 {/if}
 
-<form action="?/post" method="POST" use:enhance>
-	Comment:
-	<input type="text" name="message" />
-	<button type="submit">Submit</button>
+<form action="?/post" method="POST" use:enhance class="space-y-2">
+	<label for="message">Comment:</label>
+	<div class="md:flex gap-2">
+		<input id="message" type="text" name="message" class="border py-1.5 px-2.5 rounded-md w-full" />
+		<button
+			type="submit"
+			class="w-full mt-2 md:mt-0 md:w-auto border py-1.5 px-2.5 rounded-md hover:bg-gray-50"
+			>Submit</button
+		>
+	</div>
 </form>
 
-<ul>
+<ul class="list-disc list-inside">
 	{#each form?.comments || data.comments as comment}
 		<li>{comment.sender} at {comment.createdAt.toLocaleString()}: {comment.message}</li>
 	{/each}
